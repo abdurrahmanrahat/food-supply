a992cd2a84adfd236fb6e1fd21a66ed176030731
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-17 15:14:09 +0600

    login formj

---
+++ src/components/form/FSInput.tsx
@@ -0,0 +1,24 @@
+import { Input } from "antd";
+import { Controller } from "react-hook-form";
+
+type TInputProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
+
+const FSInput = ({ type, name, label }: TInputProps) => {
+  return (
+    <div className="md:text-[16px] mb-5">
+      {label ? label : null}
+      <Controller
+        name={name}
+        render={({ field }) => (
+          <Input className="text-[17px]" {...field} type={type} id={name} />
+        )}
+      />
+    </div>
+  );
+};
+
+export default FSInput;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,27 @@
+import FSInput from "@/components/form/FSInput";
+import FormWrap from "@/components/form/FormWrap";
+import { Button, Row } from "antd";
+import { FieldValues } from "react-hook-form";
+
 const Login = () => {
-  return <div>Login</div>;
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+  return (
+    <Row justify="center" align="middle" style={{ height: "100vh" }}>
+      <div className="p-8 rounded-md own-shadow">
+        <FormWrap onSubmit={onSubmit}>
+          <FSInput type="text" name="email" label="Email:" />
+
+          <FSInput type="text" name="password" label="Password:" />
+
+          <Button htmlType="submit" className="md:text-[15px]">
+            Login
+          </Button>
+        </FormWrap>
+      </div>
+    </Row>
+  );
 };
 
 export default Login;
