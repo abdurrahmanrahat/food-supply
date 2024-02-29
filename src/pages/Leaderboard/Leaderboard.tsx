77c27a8c3487cc594ed68b5bd517733ec389cd8d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-29 12:05:35 +0600

    leaderboard

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -5,7 +5,35 @@ const Leaderboard = () => {
   const { data: suppliesDonations } = useGetSupplyDonationQuery(undefined);
   console.log(suppliesDonations);
 
-  return <Container>Leaderboard</Container>;
+  return (
+    <Container className="my-16">
+      <div className="text-center mb-14 relative">
+        <span className="text-[32px] md:text-[40px] font-bold border-b-[3px] border-b-[#DABC95]">
+          Leaderboard
+        </span>
+      </div>
+
+      {/* table list */}
+      <div className="md:mx-[200px] pb-8">
+        {/* head */}
+        <div className="grid grid-cols-12 bg-gray-400 py-2 px-4 font-medium text-[18px] md:text-[20px]">
+          <div className="col-span-4">Rank</div>
+          <div className="col-span-4">Name</div>
+          <div className="col-span-4 ">Donations</div>
+        </div>
+        {/* body */}
+        <div className="my-2">
+          {suppliesDonations?.data.map((item, index) => (
+            <div className="md:text-[18px] grid grid-cols-12 pb-2 border-b border-gray-300 py-2 px-4">
+              <div className="col-span-4 font-medium">{index + 1}</div>
+              <div className="col-span-4">{item.userName}</div>
+              <div className="col-span-4">{item.newSupplyQuantity}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Leaderboard;
