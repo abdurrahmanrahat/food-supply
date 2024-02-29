12f82854af7b5fc874ea0013fc0dcf115ed6f8f6
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-29 13:48:53 +0600

    volunteer apis

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,6 +5,6 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://food-supply-server.vercel.app/api/v1",
   }),
-  tagTypes: ["supply", "donation"],
+  tagTypes: ["supply", "donation", "volunteer"],
   endpoints: () => ({}),
 });
---
+++ src/redux/features/volunteer/volunteer.api.ts
@@ -0,0 +1,24 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const volunteerApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getVolunteers: builder.query({
+      query: () => ({
+        url: "/volunteers",
+        method: "GET",
+      }),
+      providesTags: ["volunteer"],
+    }),
+
+    addVolunteer: builder.mutation({
+      query: (data) => ({
+        url: "/volunteers",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["volunteer"],
+    }),
+  }),
+});
+
+export const { useGetVolunteersQuery, useAddVolunteerMutation } = volunteerApi;
